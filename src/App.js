75fceffb82a2a1c820020329a75141fc5853d8de
Rahul Ranjan <rahulranjan5sep@gmail.com>
2023-01-16 16:06:07 +0530

    changed mappls with (radar + bing map) combo, already done the basics of marker+infoBox and its event handlers

--- src/App.js
+++ src/App.js
@@ -1,104 +1,48 @@
 import './App.css';
 import React from 'react';
-import { mappls } from  'mappls-web-maps';
+import Radar from 'radar-sdk-js';
+import { ReactBingmaps } from 'react-bingmaps';
 import { useState,useEffect } from 'react';
 
 function App() {
+  const [locationSet, setlocationSet] = useState(false);
+  const [userLocation, setuserLocation] = useState([0,0]);
   const  styleMap  = {width:  '70%', height:  '50vh', display:'inline-block'};
-  
+  const infoboxesWithPushPins = [{"location" : [26.7742,74.9646],"addHandler" : "mouseover",
+  "infoboxOption": { title: 'Infobox Title', description: 'Infobox' },
+  "pushPinOption":{ title: 'Pushpin Title', description: 'Pushpin' },
+  "infoboxAddHandler" : {"type" : "click",callback : function(){console.log("more details")}}
+}]
+
   useEffect(() => {
-    
-
-  const  mapProps  = 
-  { 
-    center: [28.6562, 77.2410], 
-    traffic:  false, 
-    zoom:  8, 
-    geolocation:  true,
-    clickableIcons:  true 
-  }
-  var mapObject ;
-  var markerObject1;
-  var mapplsClassObject=  new  mappls();
-
-  mapplsClassObject.initialize("801cb30b77463388a85b90907f366329",()=>{
-    mapObject = mapplsClassObject.Map({id:  "map", properties: mapProps});
-
-    var  geoData=
-{
-  "type":  "FeatureCollection",
-  "features": 
-    [
-      {
-        "type":  "Feature",
-        "properties":
-          {
-            "description":"noida",
-            "icon":"https://apis.mapmyindia.com/map_v3/2.png"
-          },
-        "geometry": 
-          {
-            "type":  "Point",
-            "coordinates": [28.544,77.5454]
-          }
-      },
-      {
-        "type":  "Feature",
-        "properties": 
-          {
-            "description":"faridabad",
-            "icon":"https://apis.mapmyindia.com/map_v3/2.png"
-          },
-        "geometry": 
-          {
-            "type":  "Point",
-            "coordinates": [28.27189158,77.2158203125]
-          }
-      },
-      {
-        "type":  "Feature",
-        "properties": 
-          {
-            "description":"<h1>hi</h1>",
-            "icon":"https://apis.mapmyindia.com/map_v3/1.png"
-          },
-        "geometry": 
-          {
-            "type":  "Point",
-            "coordinates": [28.549511,77.2678250]
-          }
+    Radar.initialize("prj_live_pk_ae938e2752f6e2305f036288d899e76fab8ef7d0");
+    Radar.trackOnce(function(err, result) {
+      if (!err) {
+        // do something with result.location, result.events, result.user
+        console.log(result);
+        setuserLocation([result.location.latitude,result.location.longitude]);
+        setlocationSet(true);
       }
-    ]
-};
-    markerObject1 = mapplsClassObject.addGeoJson({
-      map : mapObject,
-      data : geoData,
-      fitbounds : true,
-      cType : 0
-    })
-
-    // var infoWindow = infoWindow = mapplsClassObject.InfoWindow({
-    //   map : mapObject,
-    //   position : {lat : 28.27189158,lng : 77.2158203125},
-    //   content : "window here!!",
-    //   class : "info_class",
-    //   maxWidth : 300,
-    //   closeButton : true
-    // });
-    markerObject1.addListener('click',function(){
-      console.log('hey!!');
-    })
-
-  });
-
+    });
   }, [])
-  
-  
-    
-  return (
-    <div className="App">
-      <div id="map" style={styleMap}></div>
-      </div>
+
+
+  return (<>
+  {console.log(userLocation)}
+    {locationSet === true ?
+    <div style={styleMap}>
+      <ReactBingmaps 
+        infoboxesWithPushPins = {infoboxesWithPushPins}
+        bingmapKey = "AixTKAvEgAki5Zwsi0SV1breMlpZHUynV3HKZJEHyBjvtoymETk1rxtTw6DvBYUH"
+        center = {userLocation}> 
+      </ReactBingmaps>
+    </div>:<>process hora hai abhi</>
+    // <div className="map__container">
+    //   <BingMapsReact onMapReady={({map})=>{setmapReady(true)}}
+    //   bingMapsKey="AixTKAvEgAki5Zwsi0SV1breMlpZHUynV3HKZJEHyBjvtoymETk1rxtTw6DvBYUH" />
+    //   </div>
+}
+    </>
   );
 }
 
